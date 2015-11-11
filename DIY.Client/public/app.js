dc6979a90bad52721275e04acd762310d355bee9
ahansb <andrejboyadjiev@gmail.com>
2015-11-11 18:29:02 +0200

    initial client part

---
+++ DIY.Client/public/app.js
@@ -0,0 +1,54 @@
+(function() {
+
+  var sammyApp = Sammy('#content', function() {
+
+    this.get('#/', function(context) {
+      context.redirect('#/home');
+    });
+
+    this.get('#/home', controllers.home.all);
+    this.get('#/home/add', controllers.home.add);
+
+    this.get('#/my-cookie', controllers.myCookie.all);
+
+    this.get('#/users/register', controllers.users.register);
+  });
+
+  $(function() {
+    sammyApp.run('#/');
+
+    if (data.users.hasUser()) {
+      $('#container-sign-in').addClass('hidden');
+      $('#btn-sign-out').on('click', function() {
+        data.users.signOut()
+          .then(function() {
+            toastr.success('User signed out!');
+            setTimeout(function() {
+              document.location = '#/';
+              document.location.reload(true);
+            }, 1000);
+          });
+      });
+    } else {
+      $('#container-sign-out').addClass('hidden');
+      $('#btn-sign-in').on('click', function(e) {
+        e.preventDefault();
+        var user = {
+          username: $('#tb-username').val(),
+          password: $('#tb-password').val()
+        };
+        data.users.signIn(user)
+          .then(function(user) {
+            toastr.success('User signed in!');
+            setTimeout(function() {
+              document.location = '#/';
+              document.location.reload(true);
+            }, 1000);
+          }, function(err) {
+            $('#container-sign-in').trigger("reset");
+            toastr.error(err.responseText);
+          });
+      });
+    }
+  });
+}());
---
+++ DIY.Client/public/controllers/my-cookie.js
@@ -0,0 +1,39 @@
+var controllers = controllers || {};
+(function(scope) {
+
+  function all(context) {
+    var myCookie;
+    data.myCookies.get()
+      .then(function(resMyCookie) {
+        myCookie = resMyCookie;
+        // myCookie.timePassed =  
+        return templates.get('my-cookie');
+      }).then(function(template) {
+        context.$element().html(template(myCookie));
+        $('.btn.btn-success').on('click', function() {
+          var $this = $(this),
+            cookieId = $this.parents('.cookie-box').attr('data-id');
+          data.cookies.like(cookieId)
+            .then(function(cookie) {
+              $this.parents('.cookie-box').find('.likes').html(cookie.likes);
+              toastr.clear();
+              toastr.success('Cookie liked!');
+            });
+        });
+        $('.btn.btn-danger').on('click', function() {
+          var $this = $(this),
+            cookieId = $this.parents('.cookie-box').attr('data-id');
+          data.cookies.dislike(cookieId)
+            .then(function(cookie) {
+              toastr.clear();
+              toastr.error('Cookie disliked!');
+              $this.parents('.cookie-box').find('.dislikes').html(cookie.dislikes);
+            });
+        });
+      });
+  }
+
+  scope.myCookie = {
+    all: all,
+  };
+}(controllers));
