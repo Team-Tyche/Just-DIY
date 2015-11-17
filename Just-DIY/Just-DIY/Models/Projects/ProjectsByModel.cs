d5234a6a5661eda71d5d7a4c7b065bee15120fec
Mihaela Encheva <dyct.ent@gmail.com>
2015-11-17 17:21:00 +0200

    remove all because of merge conflicts
    everybody does whatever the F the want anyways....

--- Just-DIY/Just-DIY/Models/Projects/ProjectsByModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Just_DIY.Models.Projects
-{
-    public class ProjectsByModel
-    {
-        public string AuthorUsername { get; set; }
-    }
-}
--- Just-DIY/Just-DIY/Models/Projects/ProjectsByViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Just_DIY.Models.Projects
-{
-    public class ProjectsByViewModel
-    {
-        public int Id { get; set; }
-
-        public string Name { get; set; }
-    }
-}
